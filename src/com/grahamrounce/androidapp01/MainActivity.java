353d2c7ef35185f34dc10b243030f46d8dfccc1f
Graham Rounce <grahamrounce+github@gmail.com>
2012-08-25 18:59:35 +0100

    Added hello world and button + event listener

--- src/com/grahamrounce/androidapp01/MainActivity.java
+++ src/com/grahamrounce/androidapp01/MainActivity.java
@@ -3,18 +3,36 @@ package com.grahamrounce.androidapp01;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	TextView theTextView;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        theTextView = (TextView) findViewById(R.id.textView1);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void evt_button1_onClick(View v) {
+    	String curText = theTextView.getText().toString();
+    	
+    	if(curText == "dlroW") {
+    		curText = "World";
+    	} else {
+    		curText = "dlroW";
+    	}
+    	
+    	theTextView.setText(curText);
     }
 }
